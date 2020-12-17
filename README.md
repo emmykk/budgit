e5b250c98b66b6f1aed74d3748c6d9d64e1057ff
ursinewalrus <joshkerxhallik@gmail.com>
2020-12-16 19:29:15 -0500

    prettier

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # budgit-api
+
 No frills budget tracking by user-defined categories.
 
 # Contributor Note:
 
-Please use an *imperative* commit style!
+Please use an _imperative_ commit style!
 Write commits like this, in the imperative present tense, concisely describing the action:
-
 
 `'Add comments section to home page'`
 `'Delete questions component'`
--- db/dbService.ts
+++ db/dbService.ts
@@ -1,31 +1,27 @@
-const { Sequelize, Model, DataTypes } = require('sequelize');
-console.log( process.env.DB_CONNECTION_HOST);
+const { Sequelize, Model, DataTypes } = require("sequelize");
+console.log(process.env.DB_CONNECTION_HOST);
 const dataBase = new Sequelize({
-    database: process.env.DB_CONNECTION_DATABASE,
-    username:  process.env.DB_CONNECTION_USERNAME,
-    password:  process.env.DB_CONNECTION_PASSWORD,
-    host:  process.env.DB_CONNECTION_HOST,
-    port: 5432,
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false 
-      }
+  database: process.env.DB_CONNECTION_DATABASE,
+  username: process.env.DB_CONNECTION_USERNAME,
+  password: process.env.DB_CONNECTION_PASSWORD,
+  host: process.env.DB_CONNECTION_HOST,
+  port: 5432,
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
     },
-  });
+  },
+});
 
 const pingDb = async () => {
-    try {
-        await dataBase.authenticate(); 
-        return "DB Auth successful";
-    }
-    catch(err) {
-        return `DB Auth failed: ${err.toString()}`;
-    }
-}
+  try {
+    await dataBase.authenticate();
+    return "DB Auth successful";
+  } catch (err) {
+    return `DB Auth failed: ${err.toString()}`;
+  }
+};
 
-
-export {
-    pingDb
-}
+export { pingDb };
