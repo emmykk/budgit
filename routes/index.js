241d7639c992d437c0b1315c6db28354608117d7
Emmy K <ekcalloway@gmail.com>
2020-12-21 21:06:42 -0500

    Add authenticated GET and POST endpoints for households and expenses, and login endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,52 +1,109 @@
 var express = require("express");
 var router = express.Router();
 const { insertUser } = require("../db/dbservice.ts");
+const { addHousehold } = require("../db/api/household.js");
+const { addExpense, getAllExpenses } = require("../db/api/expense.js");
+const dbService = require("../db/dbservice.ts");
 const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-const { Console } = require("console");
+const jwt = require("jwt-simple");
 
-/* GET home page. */
 router.get("/", (req, res, next) => {
-  res.render("index", { title: "Express" });
+  res.send({ message: "" });
 });
 
 router.post("/users", (req, res, next) => {
   insertUser(req.body);
 });
 
-router.post("/login", (req, res, next) => {
-  const { username, password } = req.body;
+router.get(
+  "/households",
+  passport.authenticate("bearer", { session: false }),
+  (req, res) => {
+    if (req.user) res.send({ token: req.user, household: true });
+    else res.send({ error: "ERRROR" });
+  }
+);
 
-  if (!username)
-    return res.status(422).json({
-      errors: {
-        username: "is required",
-      },
-    });
+router.post(
+  "/households",
+  passport.authenticate("bearer", { session: false }),
+  /**
+   * @param {*} req.body expects:
+   * name - string
+   */
+  async (req, res) => {
+    const token = req.user;
+    if (req.user) {
+      const userId = jwt.decode(req.user, process.env.APP_JWT_SECRET).userId;
+      const household = { ...req.body, userId };
 
-  if (!password)
-    return res.status(422).json({
-      errors: {
-        password: "is required",
-      },
-    });
-
-  return passport.authenticate(
-    "local",
-    { session: true },
-    (err, passportUser) => {
-      if (err) {
-        console.log(err);
-        return next(err);
+      if (household.name) {
+        // const result = await dbService.getAllUsers();
+        console.log("THEE NAME");
+        const result = await addHousehold(household);
+        console.log(result);
+        res.send({ message: result });
       }
+    } else
+      res.send({ error: "You are not logged in. Please log in to continue" });
+  }
+);
 
-      if (passportUser) {
-        return res.json({ user: passportUser.dataValues.username });
+router.post(
+  "/expenses",
+  passport.authenticate("bearer", { session: false }),
+  /**
+   * @param {*} req.body expects:
+   * amount - double
+   * description - string
+   * householdId - integer (store on FE)
+   */
+  async (req, res) => {
+    if (req.user) {
+      const userId = jwt.decode(req.user, process.env.APP_JWT_SECRET).userId;
+      const expenseData = { ...req.body, UserId: userId };
+      try {
+        const result = await addExpense(expenseData);
+        res.send({ message: result });
+      } catch (err) {
+        res.send({ message: err });
       }
+    } else
+      res.send({ error: "You are not logged in. Please log in to continue." });
+  }
+);
 
-      return status(400).info;
-    }
-  )(req, res, next);
-});
+router.get(
+  "/expenses",
+  passport.authenticate("bearer", { session: false }),
+  /**
+   * Get expenses for a user's household via a lookup in HouseholdMembers with userId.
+   */
+  async (req, res) => {
+    const token = req.user;
+    if (req.user) {
+      const userId = jwt.decode(req.user, process.env.APP_JWT_SECRET).userId;
+      try {
+        const result = await getAllExpenses(userId);
+        res.send({ message: result });
+      } catch (err) {
+        res.send({ message: err });
+      }
+    } else
+      res.send({ error: "You are not logged in. Please log in to continue." });
+  }
+);
+
+router.post(
+  "/login",
+  passport.authenticate("local", { session: false }),
+  (req, res) => {
+    // authentication was successful.
+    // `req.user` contains the authenticated user.
+    console.log(req.user);
+
+    res.send({ token: req.user });
+  }
+);
 
 module.exports = router;
