da249cfd0bcc47e8d77173de1f6af654c06e90e1
Emmy K <ekcalloway@gmail.com>
2020-12-21 21:00:03 -0500

    Add migrations for adding household members table, expense fields, etc.

---
+++ db/migrations/20201221234019-update_expenses_add_description_field.js
@@ -0,0 +1,13 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn("Expenses", "description", {
+      allowNull: false,
+      type: Sequelize.TEXT,
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Expenses");
+  },
+};
